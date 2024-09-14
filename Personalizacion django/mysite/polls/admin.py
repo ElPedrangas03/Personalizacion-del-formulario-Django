6507492bf6b258c9836031b064649f48723c941e
ElPedrangas03 <toniramos2345@gmail.com>
2024-09-14 00:02:51 -0600

    Personalice el formulario del sitio administrativo

--- Personalizacion django/mysite/polls/admin.py
+++ Personalizacion django/mysite/polls/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from polls.models import Question, Choice
+from .models import Question
 
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
